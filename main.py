250f534cee21759ad9811a767b22b2642dc15ffd
eialid <Eaj@#!$567890>
2025-11-17 22:56:25 -0330

    cors enabled for frontend

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from weather_service import fetch_current_weather
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create FastAPI app
 app = FastAPI(title="St. John's Weather Alert System")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React default port
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Root endpoint
 @app.get("/")
@@ -16,3 +26,7 @@ def home():
 @app.get("/health")
 def health():
     return {"status": "healthy"}
+
+@app.get("/weather")
+def get_weather():
+    return fetch_current_weather()
